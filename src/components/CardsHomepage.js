cf9fd08f16a4d218f5e4476f05de1226c9390699
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-10-29 00:17:06 -0300

    install material-ui, set material-ui, create CardsHomepage component

---
+++ src/components/CardsHomepage.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import photoChamada from "../photo-chamada.jpg";
+import photoCard from "../photo-card.jpg";
+import photoManifesto from "../photo-manifesto.jpg"
+import book from "../../src/capa-de-livro.jpg";
+import ceramica from '../../src/ceramica.jpg';
+import tela from '../../src/tela.jpg';
+
+//------ material ui -------
+
+import { makeStyles } from "@material-ui/core/styles"
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+
+    
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={ceramica}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+    
+  );
+}
+
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,39 +2,68 @@ import React from "react";
 import { Link } from "react-router-dom";
 import photoChamada from "../photo-chamada.jpg";
 import photoCard from "../photo-card.jpg";
-import photoManifesto from "../photo-manifesto.jpg"
+import photoManifesto from "../photo-manifesto.jpg";
 import book from "../../src/capa-de-livro.jpg";
-import ceramica from '../../src/ceramica.jpg';
-import tela from '../../src/tela.jpg';
+import ceramica from "../../src/ceramica.jpg";
+import tela from "../../src/tela.jpg";
+
+import CardsHomepage from "./CardsHomepage";
+
+//------ Material Ui -------
+
+import { Grid } from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: '20px',
+    paddingRight: '20px'
+  }
+});
 
 function Homepage() {
+  const classes = useStyles();
+
   return (
     <div className="homepage-container container-fluid p-0">
-      
       <div className="homepage-main container-fluid d-flex justify-content-center align-items-center ">
-      
         <div className="row">
+          <div className="col-2 text-right pr-0 ">
+            <h1 className="h1-chamada"> CONTRA </h1>
+          </div>
+          {/* <div className="col-8 div-image-chamada pl-0"></div> */}
+          <div className="col-10 div-image-chamada pl-0">
+            <img
+              className="photo-chamada"
+              src={photoChamada}
+              alt="mulher deitada no chão pintando"
+            />
+            <div className="h1-over-image-chamada"> FLUXO </div>
+            <span className="lugar-certo">O lugar certo</span>
+            <span className="para-artistas-independentes">
+              para
+              <span className="destaque-amarelo"> artistas independentes.</span>
+            </span>
+          </div>
 
-            <div className="col-2 text-right pr-0 ">
-                  <h1 className="h1-chamada"> CONTRA </h1>
-                 
-
-                 
-            </div>
-           {/* <div className="col-8 div-image-chamada pl-0"></div> */}
-            <div className="col-10 div-image-chamada pl-0">
-                <img className="photo-chamada" src={photoChamada} alt="mulher deitada no chão pintando"/>
-                <div className="h1-over-image-chamada"> FLUXO </div>
-                    <span className="lugar-certo">O lugar certo</span>
-                    <span className="para-artistas-independentes">para<span className="destaque-amarelo"> artistas independentes.</span></span>
-            </div>
-          
           <img />
         </div>
       </div>
 
-      <div className="homepage-destaques container-fluid">
-        {/*d-flex  justify-content-around align-items-center*/}
+      <Grid container spacing={4} className={classes.gridContainer} align-items-center="center">
+        <Grid item xs={12} sm={6} md={4} lg={4}>
+          <CardsHomepage />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={4}>
+          <CardsHomepage />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={4}>
+          <CardsHomepage />
+        </Grid>
+      </Grid>
+      {/* <div className="homepage-destaques container-fluid">
+        
   <div className="homepage-destaques-two row align-items-center">
           <div className="col-1">
             <p className='destaques-p-left'>Destaques da semana</p>
@@ -44,7 +73,7 @@ function Homepage() {
           <div className="col-10 d-flex justify-content-around">
 
           <div className="card ">
-            <img className="card-img-top" src={book} alt="Card image cap" />
+            <img className="card-img-top" src={book} alt="Card  cap" />
             <div className="card-body">
               <h5 className="card-title">Livro</h5>
               <p className="card-text card-home ">
@@ -56,7 +85,7 @@ function Homepage() {
             </div>
           </div>
           <div className="card">
-            <img className="card-img-top" src={ceramica} alt="Card image cap" />
+            <img className="card-img-top" src={ceramica} alt="Card cap" />
             <div className="card-body">
               <h5 className="card-title">Escultura</h5>
               <p className="card-text card-home">
@@ -68,7 +97,7 @@ function Homepage() {
             </div>
           </div>
           <div className="card">
-            <img className="card-img-top" src={tela} alt="Card image cap" />
+            <img className="card-img-top" src={tela} alt="Card  cap" />
             <div className="card-body">
               <h5 className="card-title">Pintura em tela</h5>
               <p className="card-text card-home">
@@ -79,9 +108,7 @@ function Homepage() {
               </div>
             </div>
           </div>
-            {/* <div className="col-1">
-            <p className='destaques-p-right'>Destaques da semana</p>
-          </div> */}
+        
 
           </div>
         <div className="col-1">
@@ -89,24 +116,36 @@ function Homepage() {
             <p className='destaques-p-right'>Destaques da semana</p>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="homepage-manifest">
         <h1>Manifesto</h1>
         <hr className="hr-manifesto" />
 
         <div className="div-manifesto container-fluid d-flex justify-content-center align-items-center  ">
-
           <div className="p-manifesto ">
-
-        <p>
-        Somos uma plataforma que não<br/> só valoriza os artistas<br/> independentes mas que também<br/> possibilita a divulgação e venda<br/> de suas artes.<br/>
-Se você é alguém que valoriza<br/> esse tipo de conteúdo, ou alguém<br/> que já teve coragem o suficiente<br/> para mostrar ao sistema que você<br/> consegue<br/> 
-criar e produzir algo fora dele, aqui<br/> é seu lugar! Junte-se a nós e faça<br/> parte desse movimento<br/> <span className='destaque-amarelo'>Contra Fluxo</span>        </p>
+            <p>
+              Somos uma plataforma que não
+              <br /> só valoriza os artistas
+              <br /> independentes mas que também
+              <br /> possibilita a divulgação e venda
+              <br /> de suas artes.
+              <br />
+              Se você é alguém que valoriza
+              <br /> esse tipo de conteúdo, ou alguém
+              <br /> que já teve coragem o suficiente
+              <br /> para mostrar ao sistema que você
+              <br /> consegue
+              <br />
+              criar e produzir algo fora dele, aqui
+              <br /> é seu lugar! Junte-se a nós e faça
+              <br /> parte desse movimento
+              <br /> <span className="destaque-amarelo">Contra Fluxo</span>{" "}
+            </p>
           </div>
-        
-        <img className='photo-manifesto' src={photoManifesto} />
+
+          <img className="photo-manifesto" src={photoManifesto} />
         </div>
-      </div> 
+      </div>
     </div>
   );
 }
